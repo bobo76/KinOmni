e30492469706db2770c8761d0903571e1c81348a
Boris Proulx <boris.proulx@telus.com>
2020-02-03 08:03:46 -0500

    bit more...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44353'
+  apiUrl: "https://localhost:44353"
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -10,4 +10,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(_ => console.log(`Bootstrap success`))
   .catch(err => console.error(err));
